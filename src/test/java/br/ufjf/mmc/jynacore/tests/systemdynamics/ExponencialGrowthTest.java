4d35d924451481a454942457b8d733c52bb27b3b
igorknop <igorknop@gmail.com>
2012-09-26 16:59:35 -0300

    Fix the growth model test.
    The assertions must be done with an error diference, not equality

--- src/test/java/br/ufjf/mmc/jynacore/tests/systemdynamics/ExponencialGrowthTest.java
+++ src/test/java/br/ufjf/mmc/jynacore/tests/systemdynamics/ExponencialGrowthTest.java
@@ -1,20 +1,21 @@
-/*******************************************************************************
- * Copyright (c) 2009 Igor Knop.
- *     This file is part of JynaCore.
- * 
- *     JynaCore is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- * 
- *     JynaCore is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Lesser General Public License for more details.
- * 
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with JynaCore.  If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+/**
+ * *****************************************************************************
+ * Copyright (c) 2009 Igor Knop. This file is part of JynaCore.
+ *
+ * JynaCore is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * JynaCore is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JynaCore. If not, see <http://www.gnu.org/licenses/>.
+ * ****************************************************************************
+ */
 package br.ufjf.mmc.jynacore.tests.systemdynamics;
 
 import static org.junit.Assert.assertEquals;
@@ -36,63 +37,58 @@ import br.ufjf.mmc.jynacore.systemdynamics.simulator.impl.DefaultSystemDynamicsS
 
 public class ExponencialGrowthTest {
 
-	private SystemDynamicsModel model;
+   private SystemDynamicsModel model;
 
-	@Before
-	public void setUp() throws Exception {
-		model = new ExponencialGrowth();
-	}
+   @Before
+   public void setUp() throws Exception {
+      model = new ExponencialGrowth();
+   }
 
-	@Test
-	public void TestRun() throws Exception {
+   @Test
+   public void TestRun() throws Exception {
 
-		model = new ExponencialGrowth();
-		JynaSimulation simulation = new DefaultSystemDynamicsSimulation();
-		JynaSimulationMethod method = new DefaultSystemDynamicsEulerMethod();
-		JynaSimulationProfile profile = new DefaultSimulationProfile();
-		JynaSimulationData data = new DefaultSimulationData();  
+      model = new ExponencialGrowth();
+      JynaSimulation simulation = new DefaultSystemDynamicsSimulation();
+      JynaSimulationMethod method = new DefaultSystemDynamicsEulerMethod();
+      JynaSimulationProfile profile = new DefaultSimulationProfile();
+      JynaSimulationData data = new DefaultSimulationData();
 
-		simulation.setProfile(profile);
-		simulation.setSimulationData(data);
-		simulation.setMethod(method);
-		simulation.setModel(model);
+      simulation.setProfile(profile);
+      simulation.setSimulationData(data);
+      simulation.setMethod(method);
+      simulation.setModel(model);
 
-		simulation.reset();
-		simulation.run();
+      profile.setTimeSteps(2);
+      simulation.reset();
+      simulation.run();
 
-		assertEquals((Double) 2.25, ((InformationSource) model.get("Population"))
-				.getValue());
-		System.out.printf("%f %f\n", simulation.getTime(), ((InformationSource) model.get("Population"))
-				.getValue());
+      assert (Math.abs((Double) 2.25 - ((InformationSource) model.get("Population")).getValue()) < 0.001);
+      System.out.printf("%f %f\n", simulation.getTime(), ((InformationSource) model.get("Population"))
+              .getValue());
 
-		simulation.getProfile().setTimeSteps(8);
-		simulation.run();
-		assertEquals((Double) 57.665, ((InformationSource) model.get("Population"))
-				.getValue());
-		System.out.printf("%f %f\n", simulation.getTime(), ((InformationSource) model.get("Population"))
-				.getValue());
-		
-		simulation.getProfile().setTimeSteps(10);
-		simulation.run();
-		assertEquals((Double) 3325.26, ((InformationSource) model.get("Population"))
-				.getValue());
-		System.out.printf("%f %f\n", simulation.getTime(), ((InformationSource) model.get("Population"))
-				.getValue());
+      simulation.getProfile().setTimeSteps(8);
+      simulation.run();
+      assert (Math.abs((Double) 57.6650390625 - ((InformationSource) model.get("Population")).getValue()) < 0.001);
+      System.out.printf("%f %f\n", simulation.getTime(), ((InformationSource) model.get("Population"))
+              .getValue());
 
-		simulation.getProfile().setTimeSteps(30);
-		simulation.run();
-		assertEquals((Double) 6.376215002140496e8, ((InformationSource) model.get("Population"))
-				.getValue());
-		System.out.printf("%f %f\n", simulation.getTime(), ((InformationSource) model.get("Population"))
-				.getValue());
-		
-		simulation.getProfile().setTimeSteps(50);
-		simulation.run();
-		assertEquals((Double) 4.0656117753521523e17, ((InformationSource) model.get("Population"))
-				.getValue());
-		System.out.printf("%f %f\n", simulation.getTime(), ((InformationSource) model.get("Population"))
-				.getValue());
-		
-	}
+      simulation.getProfile().setTimeSteps(10);
+      simulation.run();
+      assert (Math.abs((Double) 3325.2567300797 - ((InformationSource) model.get("Population")).getValue()) < 0.001);
+      System.out.printf("%f %f\n", simulation.getTime(), ((InformationSource) model.get("Population"))
+              .getValue());
 
+      simulation.getProfile().setTimeSteps(30);
+      simulation.run();
+      assert (Math.abs((Double) 637621500.21405 - ((InformationSource) model.get("Population")).getValue()) < 0.001);
+      System.out.printf("%f %f\n", simulation.getTime(), ((InformationSource) model.get("Population"))
+              .getValue());
+
+      simulation.getProfile().setTimeSteps(50);
+      simulation.run();
+      assert (Math.abs((Double) 4.0656117753521523e17 - ((InformationSource) model.get("Population")).getValue()) < 0.001);
+      System.out.printf("%f %f\n", simulation.getTime(), ((InformationSource) model.get("Population"))
+              .getValue());
+
+   }
 }
